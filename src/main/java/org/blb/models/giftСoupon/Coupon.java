b9988ec02f006e945dadf8ec0e300cf4bfd2e170
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2024-07-22 19:16:06 +0200

    Entity реклама и купон

---
+++ src/main/java/org/blb/models/giftСoupon/Coupon.java
@@ -0,0 +1,22 @@
+package org.blb.models.giftСoupon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String number;
+    private String description;
+    private String discount;
+}
