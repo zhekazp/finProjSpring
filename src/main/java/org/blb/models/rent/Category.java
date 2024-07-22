392f97ee96facd924ebe3f14b5b716a666707fd9
AlinaKlochai <mrs.ali@tuta.io>
2024-07-22 15:02:57 +0200

    add entity's from rent-part with liquid base-part

---
+++ src/main/java/org/blb/models/rent/Category.java
@@ -0,0 +1,26 @@
+package org.blb.models.rent;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Product> products;
+
+}
