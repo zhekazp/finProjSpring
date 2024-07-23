57c01e0b47648e9b952830b41bc6487110cdebb9
AlinaKlochai <mrs.ali@tuta.io>
2024-07-23 14:42:31 +0200

    add exceptions

---
+++ src/main/java/org/blb/DTO/validationErrorDto/ValidationErrorsDto.java
@@ -0,0 +1,17 @@
+package org.blb.DTO.validationErrorDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ValidationErrorsDto {
+
+    private List<ValidationErrorDto> errors;
+}
