9e62f18acd7c121657f215e221603c0140854cce
zhekazp <zhekazp21@gmail.com>
2024-07-23 21:36:06 +0300

    add start controller

--- src/main/java/org/blb/controller/api/BlogApi.java
+++ src/main/java/org/blb/controller/api/BlogApi.java
@@ -4,14 +4,11 @@ import jakarta.validation.Valid;
 import org.blb.DTO.appDTO.StandardResponseDto;
 import org.blb.DTO.blog.BlogAddRequestDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/blog")
 @CrossOrigin(origins = "http://localhost:5173")
 public interface BlogApi {
-    @PostMapping("")
+    @PutMapping()
     public ResponseEntity<StandardResponseDto> addBlog(@RequestBody @Valid BlogAddRequestDTO dto);
 }
