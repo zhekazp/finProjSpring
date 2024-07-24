97dbd12f52d3e93a809b8f3d64bd6c6f3db2fd72
AlinaKlochai <mrs.ali@tuta.io>
2024-07-24 10:31:49 +0200

    dto's: product, category, and region

---
+++ src/main/java/org/blb/controller/blog/BlogsController.java
@@ -0,0 +1,22 @@
+package org.blb.controller.blog;
+
+import lombok.RequiredArgsConstructor;
+
+import org.blb.DTO.blog.BlogsRequestDTO;
+
+import org.blb.DTO.blog.blogs.BlogsResponseDTO;
+import org.blb.controller.api.BlogsApi;
+import org.blb.service.blog.BlogFindService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequiredArgsConstructor
+public class BlogsController implements BlogsApi {
+    private final BlogFindService blogFindService;
+    @Override
+    public ResponseEntity<BlogsResponseDTO> getBlogs(Integer page, Long region) {
+        return ResponseEntity.ok(blogFindService.findAll(new BlogsRequestDTO(page,region)));
+    }
+}
