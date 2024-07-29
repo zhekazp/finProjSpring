2d8dc7b04269e8635c994118552c5af7dca28861
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2024-07-29 19:17:02 +0200

    29.07.24 если в liquibase-changeLog.xml раскомментировать создание Advertising таблиц и запись в них данных, то выдает ошибку

--- src/main/java/org/blb/repository/advertising/AdvertisingRepository.java
+++ src/main/java/org/blb/repository/advertising/AdvertisingRepository.java
@@ -2,6 +2,19 @@ package org.blb.repository.advertising;
 
 import org.blb.models.advertising.Advertising;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface AdvertisingRepository extends JpaRepository<Advertising,Long> {
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AdvertisingRepository extends JpaRepository<Advertising, Long> {
+    Optional<Advertising> findByAdvertiserEmail(String email);
+
+    @Query("SELECT a FROM Advertising a WHERE a.endData > :endDate AND a.advertisingCounter > :counter")
+    List<Advertising> findAllForThoseWhoHaveNotExpiredAndCounterBiggerZero(@Param("endDate") LocalDate endDate, @Param("counter") Integer counter);
 }
---
+++ src/main/java/org/blb/service/util/AdvertisingConverter.java
@@ -0,0 +1,67 @@
+package org.blb.service.util;
+
+import org.blb.DTO.advertisingDto.AdvertisingRequestDto;
+import org.blb.DTO.advertisingDto.AdvertisingResponseDto;
+import org.blb.DTO.advertisingDto.ShortAdvertisingResponseDto;
+import org.blb.models.advertising.Advertising;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdvertisingConverter {
+    public Advertising fromDtoToEntity(AdvertisingRequestDto advertisingRequestDto) {
+        Advertising advertising = new Advertising();
+        if (advertisingRequestDto.getTitle() != null) {
+            advertising.setTitle(advertisingRequestDto.getTitle());
+        }
+
+        if (advertisingRequestDto.getDescription() != null) {
+            advertising.setDescription(advertisingRequestDto.getDescription());
+        }
+
+        if (advertisingRequestDto.getAdvertiserName() != null) {
+            advertising.setAdvertiserName(advertisingRequestDto.getAdvertiserName());
+        }
+        if (advertisingRequestDto.getAdvertiserEmail() != null) {
+            advertising.setAdvertiserEmail(advertisingRequestDto.getAdvertiserEmail());
+        }
+        if (advertisingRequestDto.getAdvertiserPhone() != null) {
+            advertising.setAdvertiserPhone(advertisingRequestDto.getAdvertiserPhone());
+
+        }
+        if (advertisingRequestDto.getDiscount() != null) {
+            advertising.setDiscount(advertisingRequestDto.getDiscount());
+        }
+        if (advertisingRequestDto.getCreateData() != null) {
+            advertising.setCreateData(advertisingRequestDto.getCreateData());
+        }
+        if(advertising.getDescriptionOfTheCoupon()!= null){
+            advertising.setDescriptionOfTheCoupon(advertising.getDescriptionOfTheCoupon());
+        }
+        return advertising;
+    }
+    public AdvertisingResponseDto fromEntityToDto(Advertising advertising) {
+        return new AdvertisingResponseDto(
+                advertising.getId(),
+                advertising.getTitle(),
+                advertising.getDescription(),
+                advertising.getAdvertiserName(),
+                advertising.getAdvertiserEmail(),
+                advertising.getAdvertiserPhone(),
+                advertising.getDiscount(),
+                advertising.getCreateData(),
+                advertising.getEndData()
+
+        );
+
+    }
+
+    public ShortAdvertisingResponseDto shortAdvertisingFromEntityToDto(Advertising advertising) {
+
+        return new ShortAdvertisingResponseDto(
+                advertising.getId(),
+                advertising.getTitle(),
+                advertising.getDescription()
+        );
+    }
+
+}
