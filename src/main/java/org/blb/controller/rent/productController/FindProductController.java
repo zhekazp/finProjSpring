4d84a80a47112043046df2671408210ddf689067
AlinaKlochai <mrs.ali@tuta.io>
2024-07-29 23:31:44 +0200

    edit FindProductService, add pagination and sort

--- src/main/java/org/blb/controller/rent/productController/FindProductController.java
+++ src/main/java/org/blb/controller/rent/productController/FindProductController.java
@@ -2,13 +2,11 @@ package org.blb.controller.rent.productController;
 
 import lombok.AllArgsConstructor;
 import org.blb.DTO.rent.ProductSearchResponse;
+import org.blb.DTO.rent.productDto.ProductResponseDto;
 import org.blb.service.rent.productServise.FindProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -22,12 +20,19 @@ public class FindProductController{
     public ResponseEntity<ProductSearchResponse> findProducts(
             @RequestParam(required = false) String region,
             @RequestParam(required = false) String category,
-            @RequestParam(required = false) String name) {
-        ProductSearchResponse response = findProductService.findProducts(region, category, name);
+            @RequestParam(required = false) String name,
+            @RequestParam(defaultValue = "0") int page) {
+        ProductSearchResponse response = findProductService.findProducts(region, category, name, page);
 
         if (response.getError() != null) {
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
-            return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+        ProductResponseDto product = findProductService.findProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
