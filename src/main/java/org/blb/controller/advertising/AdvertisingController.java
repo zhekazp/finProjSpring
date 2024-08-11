0e54687f11bf70b44957e62b6a3c7a4db1d40d42
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2024-08-11 13:50:57 +0200

    перенос с деплоя на финал

--- src/main/java/org/blb/controller/advertising/AdvertisingController.java
+++ src/main/java/org/blb/controller/advertising/AdvertisingController.java
@@ -13,124 +13,20 @@ import org.blb.controller.api.advertising.DeleteAdvertising;
 import org.blb.service.advertising.CreateAdvertisingService;
 import org.blb.service.advertising.DeleteAdvertisingService;
 import org.blb.service.advertising.FindAdvertisingService;
+import org.blb.service.startTest.StartAdvertisingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-///////////////////////////////////////////////////////////////////////////////////////////
-
-//@RestController
-//
-//@RequiredArgsConstructor
-//public class AdvertisingController
-//     implements AdvertisingApi, DeleteAdvertising, AddAdvertising
-//{
-//
-//    private final CreateAdvertisingService createAdvertisingService;
-//    private final DeleteAdvertisingService deleteAdvertisingService;
-//    private final FindAdvertisingService findAdvertisingService;
-//
-////    @PostMapping
-//    public ResponseEntity<AdvertisingResponseDto> addAdvertising(@RequestBody AdvertisingRequestDto requestDto) {
-//        AdvertisingResponseDto responseDto = createAdvertisingService.addAdvertising(requestDto);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-////    @DeleteMapping("/{id}")
-//    public ResponseEntity<StandardResponseDto> deleteAdvertising(@PathVariable Long id) {
-//        deleteAdvertisingService.deleteAdvertising(id);
-//        return ResponseEntity.noContent().build();
-//    }
-//
-////    @GetMapping("/email/{email}")
-//    public ResponseEntity<AdvertisingResponseDto> findByAdvertiserEmail(@PathVariable String email) {
-//        AdvertisingResponseDto responseDto = findAdvertisingService.findByAdvertiserEmail(email);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-////    @GetMapping
-////    public ResponseEntity<List<ShortAdvertisingResponseDto>> findAll() {
-////        List<ShortAdvertisingResponseDto> responseDtos = findAdvertisingService.findAll();
-////        return ResponseEntity.ok(responseDtos);
-////    }
-//
-////    @GetMapping("/{id}")
-//    public ResponseEntity<AdditionalResponseDto> findAdvertisingById(@PathVariable Long id) {
-//        AdditionalResponseDto responseDto = findAdvertisingService.findAdvertisingById(id);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-////    @GetMapping()
-//    public ResponseEntity<List<ShortAdvertisingResponseDto>> findFiveRandomAdvertisings() {
-//        List<ShortAdvertisingResponseDto> responseDtos = findAdvertisingService.findFiveRandomAdvertisings();
-//        return ResponseEntity.ok(responseDtos);
-//    }
-//}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-//@RestController
-//
-//@RequiredArgsConstructor
-//@RequestMapping("/ads")
-//public class AdvertisingController
-//        //implements AdvertisingApi, DeleteAdvertising, AddAdvertising
-//{
-//
-//    private final CreateAdvertisingService createAdvertisingService;
-//    private final DeleteAdvertisingService deleteAdvertisingService;
-//    private final FindAdvertisingService findAdvertisingService;
-//
-//    //    @PutMapping
-//    public ResponseEntity<AdvertisingResponseDto> addAdvertising(@RequestBody AdvertisingRequestDto requestDto) {
-//        AdvertisingResponseDto responseDto = createAdvertisingService.addAdvertising(requestDto);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-//       @DeleteMapping("/{id}")
-//    public ResponseEntity<StandardResponseDto> deleteAdvertising(@PathVariable Long id) {
-//        deleteAdvertisingService.deleteAdvertising(id);
-//        return ResponseEntity.noContent().build();
-//    }
-//
-//    @GetMapping("/email/{email}")
-//    public ResponseEntity<AdvertisingResponseDto> findByAdvertiserEmail(@PathVariable String email) {
-//        AdvertisingResponseDto responseDto = findAdvertisingService.findByAdvertiserEmail(email);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-//
-//
-//    // @CrossOrigin(origins = "http://localhost:5173")
-//    @GetMapping("/{id}")
-//    public ResponseEntity<AdditionalResponseDto> findAdvertisingById(@PathVariable Long id) {
-//        AdditionalResponseDto responseDto = findAdvertisingService.findAdvertisingById(id);
-//        return ResponseEntity.ok(responseDto);
-//    }
-//
-//
-//    // @CrossOrigin(origins = "http://localhost:5173")
-//    @GetMapping()
-//    public ResponseEntity<List<ShortAdvertisingResponseDto>> findFiveRandomAdvertisings() {
-//        List<ShortAdvertisingResponseDto> responseDtos = findAdvertisingService.findFiveRandomAdvertisings();
-//        return ResponseEntity.ok(responseDtos);
-//    }
-//
-//
-//}
-//
-//
 @RestController
 @RequiredArgsConstructor
 public class AdvertisingController implements AdvertisingApi, DeleteAdvertising, AddAdvertising {
     private final CreateAdvertisingService createAdvertisingService;
     private final DeleteAdvertisingService deleteAdvertisingService;
     private final FindAdvertisingService findAdvertisingService;
-
+    private final StartAdvertisingService advertisingService;
     @Override
     public ResponseEntity<AdvertisingResponseDto> addAdvertising(@RequestBody @Valid AdvertisingRequestDto requestDto) {
         AdvertisingResponseDto responseDto = createAdvertisingService.addAdvertising(requestDto);
@@ -165,5 +61,12 @@ public class AdvertisingController implements AdvertisingApi, DeleteAdvertising,
     public ResponseEntity<List<AdvertisingResponseDto>> findAll() {
         List<AdvertisingResponseDto> responseDtos = findAdvertisingService.findAll();
         return ResponseEntity.ok(responseDtos);
+    }
+
+    @Override
+    public ResponseEntity<String> startAdvertisements() {
+
+        advertisingService.createAdvertising();
+        return ResponseEntity.ok("Advertisements created");
     }
 }
